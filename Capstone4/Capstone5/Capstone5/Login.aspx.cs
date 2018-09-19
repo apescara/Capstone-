e7a2e0ae957b1c356740b18cee2b5fc5d74ed4c8
Erfuentes <42722205+Erfuentes@users.noreply.github.com>
2018-09-19 19:47:47 -0300

    capstone5 -
    todos los cambios dieciocheros

---
+++ Capstone4/Capstone5/Capstone5/Login.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Capstone5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnIngresar_Click(object sender, EventArgs e)
+        {
+            string user = txtUsuario.Text;
+            string password = txtPassword.Text;
+            string userName = "ernesto";
+            string passName = "123";
+            if(user.Equals(userName) && password.Equals(passName)){
+                Response.Write("<script>alert('Usuario correcto')</script>");
+                Response.Redirect("PanelGeneral.aspx");
+            }
+            else
+            {
+                Response.Write("<script>alert('Usuario incorrecto')</script>");
+            }
+        }
+    }
+}
